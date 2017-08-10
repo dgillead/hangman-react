87ca15ac3a94afb6c4a2168c040707141c65f361
Darnell Gillead <dlgillead@gmail.com>
2017-08-10 15:23:20 -0500

    set puzzleWord state on App to be random word from words array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WORDS from './words'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      puzzleWord: WORDS[Math.floor(Math.random()*WORDS.length)],
+      guessedLetters: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +20,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h1>{this.state.puzzleWord}</h1>
         </p>
       </div>
     );
